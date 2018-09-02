6695958ec2a71497ea13be8ea76e33cfd4adf7f9
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2018-09-01 22:14:52 -0700

    Organize ESS per groups, add burnin (of 25% as default)

--- src/main/java/mcli/ComputeESS.java
+++ src/main/java/mcli/ComputeESS.java
@@ -1,41 +1,71 @@
 package mcli;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
+
+import com.google.common.base.Joiner;
 
 import bayonet.math.EffectiveSampleSize;
 import blang.inits.Arg;
 import blang.inits.DefaultValue;
 import blang.inits.experiments.Experiment;
 import briefj.BriefIO;
+import briefj.BriefMaps;
 
 public class ComputeESS extends Experiment 
 {
-  @Arg 
-  File csvFile;
+  @Arg(description = "csv file containing samples")
+  public File inputFile;
   
-  @Arg 
-  Optional<String> field = Optional.empty();
+  @Arg(description = "Look for this column name for samples.") 
+                  @DefaultValue("value")
+  public String samplesColumn = "value";
   
-  @Arg @DefaultValue("1")
-  int moment        = 1;
+  @Arg(description = "Look for this column name for iteration index (or empty if none).")
+                         @DefaultValue("sample")
+  public String iterationIndexColumn = "sample";
+  
+  @Arg        @DefaultValue("1")
+  public int moment        = 1;
+         
+  @Arg            @DefaultValue("0.25")
+  public double burnInFraction = 0.25;
+  
+  @Arg     @DefaultValue("ess.csv")
+  public String output = "ess.csv";
 
   @Override
-  public void run() 
+  public void run() { try { computeEss(); } catch (Exception e) { throw new RuntimeException(e); } }
+    
+  public void computeEss() throws IOException
   {
-    List<Double> samples = new ArrayList<>();
-    if (field.isPresent())
-      for (Map<String,String> line : BriefIO.readLines(csvFile).indexCSV())
-        samples.add(Double.parseDouble(line.get(field.get()).trim()));
-    else  
-      for (String line : BriefIO.readLines(csvFile).filter(line -> !line.trim().isEmpty()))
-        samples.add(Double.parseDouble(line.trim()));
-    System.out.println(moment == 1 ?
-      EffectiveSampleSize.ess(samples) :
-      EffectiveSampleSize.ess(samples, x -> Math.pow(x, moment)));
+    if (burnInFraction < 0.0 || burnInFraction > 1.0)
+      throw new RuntimeException();
+    Map<Map<String,String>,List<Double>> samples = new LinkedHashMap<>();
+    for (Map<String,String> line : BriefIO.readLines(inputFile).indexCSV()) {
+      double value = Double.parseDouble(line.get(samplesColumn).trim());
+      line.remove(samplesColumn);
+      line.remove(iterationIndexColumn);
+      BriefMaps.getOrPutList(samples, line).add(value);
+    }
+    BufferedWriter writer = results.getAutoClosedBufferedWriter(output);
+    {
+      List<String> header = new ArrayList<>(samples.keySet().iterator().next().keySet());
+      header.add("ess");
+      writer.append(Joiner.on(",").join(header) + "\n");
+    }
+    for (Map<String,String> key : samples.keySet()) {
+      List<Double> curSamples = samples.get(key);
+      double ess = EffectiveSampleSize.ess(curSamples.subList((int) (burnInFraction * curSamples.size()), curSamples.size()), x -> Math.pow(x, moment)); 
+      List<String> entries = new ArrayList<>(key.values());
+      entries.add("" +  ess);
+      writer.append(Joiner.on(",").join(entries) + "\n");
+    }
   }
 
   public static void main(String [] args) 
